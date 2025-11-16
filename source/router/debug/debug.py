067ad0a7175032d689261aca1873ae3d2504b444
daheung <saasaa001@naver.com>
2025-11-17 06:56:28 +0900

    - Add debug endpoint for inspecting detailed server status

--- source/router/debug/debug.py
+++ source/router/debug/debug.py
@@ -6,7 +6,7 @@ import logging
 import numpy as np
 
 from PIL import Image
-from typing import cast
+from typing import cast, Dict, Optional
 from fastapi import status
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import Response, JSONResponse
@@ -25,6 +25,7 @@ from source.router.utils.verify import (
 )
 
 from source.engine.arvane_engine import ArvaneEngine
+from source.runtime.device.device_manager import DeviceDescriptor
 from source.runtime.infer.store import (
     InferenceChunkStoreConcurrent,
     EStoreOperatorType,
@@ -62,8 +63,9 @@ async def depth(
     buffer_b64 = payload.depth.buffer_b64
     depth = base64.b64decode(buffer_b64)
     depth = np.frombuffer(depth, dtype=np.uint8)
-    depth = cv2.imdecode(depth, cv2.IMREAD_UNCHANGED)
-
+    depth = cv2.imdecode(depth, cv2.IMREAD_ANYDEPTH)
+    depth = depth.astype(dtype=np.float32) / 1000
+    
     arvane_engine.container.update_object_by_task_id(
         task_id,
         objects={
@@ -73,3 +75,36 @@ async def depth(
     )
 
     return JSONResponse({ "result": "ok" }, status_code=status.HTTP_200_OK)
+
+@debug_router.get("/detail")
+async def debug_detail(
+    arvane_engine: ArvaneEngine = Depends(get_arvane_engine)
+) -> Response:
+    arvane_engine.device_manager._gpu_update()
+
+    ret_json: Dict = dict()
+    num_gpus: int = arvane_engine.device_manager.gpu_num()
+    for idx in range(num_gpus):
+        descriptor: Optional[DeviceDescriptor] = arvane_engine.device_manager.gpu_desc(idx)
+        if (descriptor is None):
+            continue
+
+        total_video_memory_per_mib: int = int(descriptor.dedicated_video_memory.total_memory / (1024 ** 2))
+        free_video_memory_per_mib: int = int(descriptor.dedicated_video_memory.free_memory / (1024 ** 2))
+        used_video_memory_per_mib: int = int(descriptor.dedicated_video_memory.used_memory / (1024 ** 2))
+        ret_json[str(idx)] = {
+            "description": descriptor.description,
+            "video_memory": {
+                "total": total_video_memory_per_mib,
+                "free": free_video_memory_per_mib,
+                "used": used_video_memory_per_mib
+            }
+        }
+
+    return JSONResponse(ret_json, status_code=status.HTTP_200_OK)
+
+@debug_router.get("/users")
+async def debug_users(
+    arvane_engine: ArvaneEngine = Depends(get_arvane_engine)
+) -> Response:
+    ...
